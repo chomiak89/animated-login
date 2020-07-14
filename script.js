ed191565ae5269d66254dadf0c32afeedee34ded
Kamil Chomiak <kchomiak@outlook.com>
2020-07-13 21:51:09 -0500

    Added checks to keep transitions in place on inputs if value is entered

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const username_field = document.querySelector("#username-input");
 const password_field = document.querySelector("#password-input");
+const username_field_before = document.querySelector(".test-selector-1");
+const password_field_before = document.querySelector(".test-selector-2");
+
+username_field.addEventListener("focusout", () => {
+  username_field.value.length > 0
+    ? username_field_before.classList.add("js-field-active")
+    : username_field_before.classList.remove("js-field-active");
+});
+password_field.addEventListener("focusout", () => {
+  password_field.value.length > 0
+    ? password_field_before.classList.add("js-field-active")
+    : password_field_before.classList.remove("js-field-active");
+});
